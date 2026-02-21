82bc7bf18985e333d4919ebb829994a74e151284
rahulprasad1399 <rahul.prasad@geekywolf.com>
2026-02-21 21:42:05 +0530

    older approach of using ngIf and ngFor

--- 2.Angular-Essentials/src/app/app.component.ts
+++ 2.Angular-Essentials/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { User } from './Model/user.model';
 import { TaskComponent } from "./task/task.component";
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
